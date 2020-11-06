c158891782c3bddc47679f6990b1f25c1b83fc2f
Wenyuan Wu <wenyuan.wu@uzh.ch>
2020-11-06 17:37:41 +0100

    bpe dropout: first stage

--- preprocess_bpe_dropout_data.sh
+++ preprocess_bpe_dropout_data.sh
@@ -1,6 +1,6 @@
-mkdir -p bpe
-cp -r baseline/preprocessed_data bpe/preprocessed_data
-cd bpe/preprocessed_data || exit
+mkdir -p bpe_dropout
+cp -r baseline/preprocessed_data bpe_dropout/preprocessed_data
+cd bpe_dropout/preprocessed_data || exit
 cp train.de train.de.bk
 cp train.en train.en.bk
 cp test.de test.de.bk
@@ -12,6 +12,7 @@ cp tiny_train.en tiny_train.en.bk
 
 subword-nmt learn-joint-bpe-and-vocab --input train.de train.en -s 4000 -o joint_de_en.code --write-vocabulary vocab.de vocab.en
 
+subword-nmt apply-bpe -c joint_de_en.code --vocabulary vocab.de --vocabulary-threshold 1 --dropout 0.1 --seed 1024 < train.de.bk > train.de.drop
 subword-nmt apply-bpe -c joint_de_en.code --vocabulary vocab.de --vocabulary-threshold 1 < train.de.bk > train.de
 subword-nmt apply-bpe -c joint_de_en.code --vocabulary vocab.en --vocabulary-threshold 1 < train.en.bk > train.en
 
@@ -25,7 +26,7 @@ subword-nmt apply-bpe -c joint_de_en.code --vocabulary vocab.de --vocabulary-thr
 subword-nmt apply-bpe -c joint_de_en.code --vocabulary vocab.en --vocabulary-threshold 1 < tiny_train.en.bk > tiny_train.en
 
 cd ../..
-mkdir -p bpe/prepared_data
-python preprocess.py --target-lang en --source-lang de --dest-dir bpe/prepared_data/ --train-prefix bpe/preprocessed_data/train \
---valid-prefix bpe/preprocessed_data/valid --test-prefix bpe/preprocessed_data/test --tiny-train-prefix bpe/preprocessed_data/tiny_train \
+mkdir -p bpe_dropout/prepared_data
+python preprocess.py --target-lang en --source-lang de --dest-dir bpe_dropout/prepared_data/ --train-prefix bpe_dropout/preprocessed_data/train \
+--valid-prefix bpe_dropout/preprocessed_data/valid --test-prefix bpe_dropout/preprocessed_data/test --tiny-train-prefix bpe_dropout/preprocessed_data/tiny_train \
 --threshold-src 1 --threshold-tgt 1 --num-words-src 4000 --num-words-tgt 4000
